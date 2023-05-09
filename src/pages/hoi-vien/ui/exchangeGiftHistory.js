d3ffea428c352d76a5121eac4fbcd11f3f45cc22
TienTein <tranxuantien.tein@gmail.com>
2023-05-09 15:39:37 +0700

    change ui by design for exchange and rotation

--- src/pages/hoi-vien/ui/exchangeGiftHistory.js
+++ src/pages/hoi-vien/ui/exchangeGiftHistory.js
@@ -1,14 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
-import Pagination from "./pagination";
 import moment from "moment/moment";
 
 function createData(id, giftTitle, createData, point) {
@@ -26,23 +17,25 @@ const ExchangeGiftHistory = () => {
   return (
     <div className="exchange__gifts">
       <h3>LỊCH SỬ ĐỔI QUÀ</h3>
-      <div className="exchange__gifts__grid exchange__gifts__header">
-        <p className="center">STT</p>
-        <p className="center">Phần thưởng</p>
-        <p className="center">Thời gian</p>
-        <p className="center">Điểm</p>
-      </div>
-      <div className="exchange__gifts__body scroll__style">
-        {rows.map((row, idx) => (
-          <div key={row.id} className="exchange__gifts__grid">
-            <p className="center">{idx + 1}</p>
-            <p>{row.giftTitle}</p>
-            <p className="center">
-              {moment(row.createData).format("M/D/YYYY h:mm:ss A")}
-            </p>
-            <p className="right">- {row.point} Fpoint</p>
-          </div>
-        ))}
+      <div className="exchange__gifts__container">
+        <div className="exchange__gifts__grid exchange__gifts__header">
+          <p className="center">STT</p>
+          <p className="center">Phần thưởng</p>
+          <p className="center">Thời gian</p>
+          <p className="center">Điểm</p>
+        </div>
+        <div className="exchange__gifts__body scroll__style">
+          {rows.map((row, idx) => (
+            <div key={row.id} className="exchange__gifts__grid">
+              <p className="center">{idx + 1}</p>
+              <p>{row.giftTitle}</p>
+              <p className="center">
+                {moment(row.createData).format("M/D/YYYY h:mm:ss A")}
+              </p>
+              <p className="right">- {row.point} Fpoint</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
